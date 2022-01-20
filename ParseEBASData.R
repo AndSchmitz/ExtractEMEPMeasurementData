df5716d509070b28e78c8d59a702934845979971
Andreas Schmitz <47260300+AndSchmitz@users.noreply.github.com>
2022-01-20 09:48:14 +0100

    No longer exclude data based on data flags.
    Instead, a flag column now exists in the output data table which can be used to filter data later.

--- ParseEBASData.R
+++ ParseEBASData.R
@@ -8,7 +8,9 @@ library(tidyverse) #data handling
 library(lubridate) #to handle dates
 
 #Define working directory here
-WorkDir <- "/home/schmitz/Downloads/test imp"
+WorkDir <- "/path/to/WorkDir"
+
+
 #EMEP data files (.nas files) must be located in one or multiple
 #sub-folders in a folder named "Input" in the working directory.
 #e.g.:
@@ -19,24 +21,10 @@ WorkDir <- "/home/schmitz/Downloads/test imp"
 #.../MyWorkDir/Input/EBASData/NH4_folder/EBAS_NH4_file_n.nas
 #.../MyWorkDir/Input/EBASData/NO3_folder/EBAS_NO3_file_1.nas
 #.../MyWorkDir/Input/EBASData/NO3_folder/EBAS_NO3_file_n.nas
-#The file "EMEP_data_flags.csv" must be located in the WorkDir/
 #Results are placed in a folder names "Output" in .../MyWorkDir/Input
 #(or change code for "InDir" and "OutDir" below for a different folder structure)
 
 
-#Define quality flags to drop
-#Rows with quality flags indicating non-valid measurements (i.e. quality flag not in category
-#"V") are deleted, with four exceptions relating to low precipitation measurements. See readme.md
-#for more information. Here, additional quality flags can be defined which will cause data to
-#be discarded if it is flagged with this values.
-QualityFlagsToDropData <- c(
-  #Explanations according to https://projects.nilu.no//ccc/flags/
-  781, 	#V 	Value below detection limit, data element contains detection limit
-  780, 	#V 	Value below detection or quantification limit, data element contains estimated or measured value. Use of flag 147 is encouraged.
-  771, 	#V 	Value above range, data element contains upper range limit
-  770 	#V 	Value above range, data element contains estimated value
-  #147 	#V 	Below theoretical detection limit or formal Q/A limit, but a value has been measured and reported and is considered valid
-)
 
 
 # --- No changes required below this line ---
@@ -50,75 +38,6 @@ dir.create(OutDir,showWarnings = F)
 #Folder for skipped files
 SkippepFilesDir <- file.path(OutDir,"SkippedFiles")
 dir.create(SkippepFilesDir,showWarnings = F)
-
-#Read explanation of EMEP data quality flag values
-EMEPFlagList <- read.table(
-  file = file.path(InDir,"EMEP_data_flags.csv"),
-  sep=";",
-  header = T,
-  skip = 2,
-  colClasses = "character"
-)
-
-
-#Helping function to interpret quality flags------
-CheckValuesAreValid <- function(ASingleFlagRow) {
-  
-  #https://projects.nilu.no//ccc/flags/
-  
-  #Must be a combination of 3-character-codes:
-  StringLength <- nchar(ASingleFlagRow)
-  if ( (StringLength %% 3) != 0 ) {
-    stop("Number of characters in quality flag must be 3,6,9,...")
-  }
-  
-  #Cut into chunks of 3 characters
-  nChunks <- StringLength/3
-  VectorOfFlags <- str_sub(
-    string = ASingleFlagRow,
-    start = seq(from = 1,to = nChunks*3, by = 3 ),
-    end = seq(from = 3,to = nChunks*3, by = 3 )
-  )
-  VectorOfFlags <- unique(VectorOfFlags)
-  
-  #Drop if any flag is in manually defined (above) list of QualityFlagsToDropData
-  if ( any(VectorOfFlags %in% QualityFlagsToDropData) ) {
-    return("NotAllValid")
-  }
-  
-  #Compare to EMEP flag list
-  if ( !all(VectorOfFlags %in% EMEPFlagList$Flag) ) {
-    stop(paste("One of the flag values not in EMEP flag list:",ASingleFlagRow))
-  }
-  Interpretation <- EMEPFlagList %>%
-    filter(
-      Flag %in% VectorOfFlags
-    ) %>%
-    distinct()
-  
-  #There are four categories of quality flags:
-  #V (valid measurement), I (invalid measurement), M (missing measurement) or H (hidden and invalid measurements)
-  #https://projects.nilu.no//ccc/flags/
-  #Multiple flags can be assigned to each value.
-  #If any of the flags is not of category "V", then discard the dataset.
-  #Exceptions: The following cases refer to measurements of precipitation and specifically
-  #to cases where low or no precipitation occurred, such that chemical analyses
-  #was not possible. These cases are no "data gaps" (not: sampling not performed). Thus, these
-  #cases are accepted but the value is set to NA and a comment is added.
-  # 784	I	Low precipitation, concentration estimated
-  # 783	M	Low precipitation, concentration unknown
-  # 782	V	Low precipitation, concentration estimated
-  # 890	M	Concentration in precipitation undefined, no precipitation
-  LowPrecipFlags <- c(784,783, 782, 890)
-  if ( any( VectorOfFlags %in% LowPrecipFlags ) ) {
-    return("LowPrecipVolume")
-  } else if ( !all(Interpretation$Category == "V") ) {
-    return("NotAllValid")
-  } else {
-    return("AllValid")
-  }
-  
-}
 
 
 #Read EMEP data-----
@@ -134,6 +53,27 @@ MetatadataKeyWords <- data.frame(
   KeyWordShort = c("StationName","code_plot","lat","lon","altitude","component","matrix","unit","InstrumentType"),
   stringsAsFactors = F
 )
+
+
+#Helping function to parse data flags------
+#Flags must be a combination of 1 or multiple 3-digit-codes, e.g. 676647392
+#Convert to a string with comma separation, e.g. "676,647,392"
+ConvertFlag <- function(ASingleFlagRow) {
+  StringLength <- nchar(ASingleFlagRow)
+  if ( (StringLength %% 3) != 0 ) {
+    stop("Number of characters in quality flag must be 3,6,9,...")
+  }
+  #Cut into chunks of 3 characters
+  nChunks <- StringLength/3
+  VectorOfFlags <- str_sub(
+    string = ASingleFlagRow,
+    start = seq(from = 1,to = nChunks*3, by = 3 ),
+    end = seq(from = 3,to = nChunks*3, by = 3 )
+  )
+  VectorOfFlags <- paste(VectorOfFlags, collapse = ",")
+  return(VectorOfFlags)
+}
+
 
 #_Loop over folders------
 DataList <- list()
@@ -281,17 +221,26 @@ for ( CurrentFolder in InputFolders ) {
     #There must be a flag column
     if ( length(idxFlagCols) == 0 ) { 
       #However, this is not always true
+      file.copy(
+        from = FullPath,
+        to = file.path(SkippepFilesDir,CurrentInFile)
+      )
+      CurrentMetadata$matrix[iFile] <- "no_flag_column_found_file_skipped"
+      SkippedFilesCounter <- SkippedFilesCounter + 1
+      #jump to next file
+      next
+      #Old code
       #Add a Flag column at the end, indicating all values are OK (optimistic approach)
       #Ensure timestamp columns are still the last two columns
-      tmp2 <- (tmp[,1:nColTreat])
-      if ( nColTreat == 1 ) {
-        tmp2 <- data.frame(tmp[,1:nColTreat],stringsAsFactors = F)
-        colnames(tmp2) <- colnames(tmp)[1]
-      }
-      tmp2 <- bind_cols(tmp2,flag = rep("000",times = nrow(tmp)))
-      tmp <- bind_cols(tmp2,tmp[,c(nColTreat+1,nColTreat+2)])
-      nColTreat <- ncol(tmp) - 2
-      idxFlagCols <- which(grepl(x=colnames(tmp),pattern="flag"))
+      # tmp2 <- (tmp[,1:nColTreat])
+      # if ( nColTreat == 1 ) {
+      #   tmp2 <- data.frame(tmp[,1:nColTreat],stringsAsFactors = F)
+      #   colnames(tmp2) <- colnames(tmp)[1]
+      # }
+      # tmp2 <- bind_cols(tmp2,flag = rep("000",times = nrow(tmp)))
+      # tmp <- bind_cols(tmp2,tmp[,c(nColTreat+1,nColTreat+2)])
+      # nColTreat <- ncol(tmp) - 2
+      # idxFlagCols <- which(grepl(x=colnames(tmp),pattern="flag"))
     }
     #The last column to treat must be a flag column
     if ( !(nColTreat %in% idxFlagCols) ) stop(
@@ -334,32 +283,10 @@ for ( CurrentFolder in InputFolders ) {
     if ( all(str_sub(string = CurrentDataLong$Flag, start = 1, end = 2) == "0.") ) {
       CurrentDataLong$Flag <- gsub(x = CurrentDataLong$Flag, pattern = "^0.",replacement = "")
     }
-    #See function CheckValuesAreValid() for information which datasets are discarded / accepted.
-    CurrentDataLong$FlagStatus <- sapply(X = CurrentDataLong$Flag, FUN = CheckValuesAreValid)
-    CurrentDataLong <- CurrentDataLong %>%
-      #Drop rows where flags indicate problems (but keep flags indicating low precip volume)
-      filter(
-        FlagStatus != "NotAllValid"
-      ) %>%
-      #Treat rows where flags indicate low precip volume
-      mutate(
-        comment = case_when(
-          FlagStatus == "LowPrecipVolume" ~ "LowPrecipVolume",
-          T ~ NA_character_
-        ),
-        value = case_when(
-          FlagStatus == "LowPrecipVolume" ~ NA_character_,
-          T ~ value
-        )
-      ) 
-    CurrentDataLong <- CurrentDataLong %>%
-      select(-FlagStatus,-Flag)
-    
-    #Skip if no valid data left
-    if ( nrow(CurrentDataLong) == 0 ) {
-      next
-    }
-    
+    #Flags must be a combination of 1 or multiple 3-digit-codes, e.g. 676647392
+    #Convert to a string with comma separation, e.g. "676,647,392"
+    CurrentDataLong$Flag <- sapply(X = CurrentDataLong$Flag, FUN = ConvertFlag)
+
     
     #___Append CurrentData to overall data list-----
     CurrentDataLong$FileID <- FileCounter
@@ -405,19 +332,6 @@ if ( any(is.na(Data$substance)) ) stop("any(is.na(Data$substance))")
 Data$substance <- gsub(x=Data$substance,pattern = "\\.[0-9]+",replacement = "\\.")
 #Then delete one or multiple occurences of "."
 Data$substance <- gsub(x=Data$substance,pattern = "\\.+",replacement = "")
-#Although quality flags have been used to exclude invalid data, some values are still
-#99.999.., 999.9999, 9.99, 9-99999, 9999.9 or similar, indicating missing or invalid data
-#The following regular expression matches the pattern of "some number of 9s, dot, some number of 9s".
-idxDatasets999 <- which(grepl(x=Data$value,pattern=("^9+\\.9+$")))
-if ( length(idxDatasets999) > 0 ) {
-  Datasets999 <- Data[idxDatasets999,]
-  nDatasets999 <- length(idxDatasets999)
-  print(paste("Despite filtering for quality flags, there are",nDatasets999,"records where the value is 9.999, 9.9, 999.99, etc. This is",round(nDatasets999/nrow(Data)*100,2),"% of the data."))
-  print("The exact values are (only one of occurrence of each exact value):")
-  print(unique(Datasets999$value))
-  print("These datasets are deleted. Even if this affects some valid data, most of the deleted records probably represent missing values.")
-  Data <- Data[-idxDatasets999,]
-}
 
 
 #Check coords------
@@ -446,9 +360,10 @@ fwrite(
   sep=";"
 )
 
-print(paste(
-  "Skipped",SkippedFilesCounter,"file(s) because it contains data from different matrices (e.g. PM2.5 and PM10).",
-  "Processing these files is not yet implemented. All skipped files have been copied to output folder \"SkippedFiles\"."
+warning(paste(
+  "Skipped",SkippedFilesCounter,"file(s) because: (A) File contains data from different matrices (e.g. PM2.5 and PM10).",
+  "Processing these files is not yet implemented. Or (B) file does not contain a \"flag\" column with data flags.",
+  "All skipped files have been copied to output folder \"SkippedFiles\"."
 ))
 
 print(paste(
